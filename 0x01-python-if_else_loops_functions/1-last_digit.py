f9a37fcd496344ebab9b098ff80b1c62a9d66e2d
Tijani1402 <99301758+Tijani1402@users.noreply.github.com>
2022-05-31 22:18:42 +0100

    commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ if number >= 0:
     last_number = number % 10
 else:
     last_number = number % -10
-print('Last digit of {:d} is {:d} and is'.format(number, last_number), end=' ')
+print('Last digit of {:d} is {:d} and is'.format(number, last_number), end='')
 if last_number > 5:
     print('greater than 5')
 elif last_number == 0:
